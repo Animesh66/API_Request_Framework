26945cad8ae0e1e5449cf80f2d84041045e139c3
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-14 17:58:59 +0530

    API tested

---
+++ Test_Cases/Products/test_get_products.py
@@ -0,0 +1,12 @@
+import requests
+
+consumer_key = 'ck_39fa25f6979b191dc83dcbcd64162d0fdb81dd14'
+consumer_secret = 'cs_0ea26b08f4793747920bfa2fa36130502dc1d0b5'
+
+
+def test_get_products():
+    response = requests.get("https://www.cheapgiftidea.com/wp-json/wc/v3/products",
+                            auth=(consumer_key, consumer_secret))
+    print("Response code is " + str(response.status_code))
+    print(response.url)
+    print(response.headers['Content-Type'])
