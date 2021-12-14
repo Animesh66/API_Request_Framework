f33fe5dceae145fb6cbca037b274ebd7fafa1cab
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-14 18:02:34 +0530

    API tested

--- Test_Cases/Products/test_get_products.py
+++ Test_Cases/Products/test_get_products.py
@@ -8,5 +8,7 @@ def test_get_products():
     response = requests.get("https://www.cheapgiftidea.com/wp-json/wc/v3/products",
                             auth=(consumer_key, consumer_secret))
     print("Response code is " + str(response.status_code))
+    response_code = response.status_code
+    assert response_code == 200, "Response code is NOT 200"
     print(response.url)
     print(response.headers['Content-Type'])
