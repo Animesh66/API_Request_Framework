d24c1d40e9bdfbdf4e626d2260dd6e7658c87f95
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-15 14:36:02 +0530

    generator helper function added.

--- Utilities/data_generator.py
+++ Utilities/data_generator.py
@@ -11,11 +11,14 @@ def generate_email_password(domain=None, email_prefix=None):
         email_prefix = 'testuser'
 
     email_length = 10
-    email_string = "".join(random.choice(string.ascii_lowercase, k=email_length))
+    email_string = ''.join(random.choices(string.ascii_lowercase, k=email_length))
     email = email_prefix + '_' + email_string + '@' + domain
 
     password_length = 15
-    password = "".join(random.choice(string.ascii_letters, k=password_length))
+    password = ''.join(random.choices(string.ascii_letters, k=password_length))
     generator = {'email': email, 'password': password}
     return generator
 
+
+# var_ep = generate_email_password(domain="santa.com.au", email_prefix="animesh")
+# print(var_ep['email'], var_ep['password'])
