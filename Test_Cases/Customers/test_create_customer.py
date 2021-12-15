9f435b2595d230a0f1d0494b333a38e0f249b5a7
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-15 17:02:41 +0530

    generator helper function added.

--- Test_Cases/Customers/test_create_customer.py
+++ Test_Cases/Customers/test_create_customer.py
@@ -1,5 +1,9 @@
 import requests
+from Utilities.data_generator import generate_email_password
 
 
 def test_create_customer():
-    pass
+    generator = generate_email_password()
+    email = generator['email']
+    password = generator['password']
+    payload = {'email': email, 'password': password}
