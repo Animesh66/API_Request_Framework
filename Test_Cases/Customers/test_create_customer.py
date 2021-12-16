dbfe87abbb791a8a09b50cf761617581ad3174d2
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-16 12:17:05 +0530

    helper function added.

--- Test_Cases/Customers/test_create_customer.py
+++ Test_Cases/Customers/test_create_customer.py
@@ -1,7 +1,7 @@
 from Helpers.Customer_Helper import CustomerHelper
 
 
-def test_create_customer():
+def test_create_customer(woo_api_test):
     customer_helper = CustomerHelper()
     customer_helper.create_customer()
     customer_helper.verify_created_email()
---
+++ Test_Cases/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture()
+def woo_api_test():
+    pass
