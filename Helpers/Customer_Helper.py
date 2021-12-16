39262a24bb9b2e7c6d232f2d64a33cd6827022a8
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-16 12:06:55 +0530

    helper function added.

--- Helpers/Customer_Helper.py
+++ Helpers/Customer_Helper.py
@@ -25,3 +25,5 @@ class CustomerHelper:
     def verify_created_email(self):
         assert self.email == self.response_json['email'], f"The given email {self.email} " \
                                                         f"is matching the response email {self.response_json['email']}"
+        print(self.email)
+        print(self.response_json['email'])
--- Test_Cases/Customers/test_create_customer.py
+++ Test_Cases/Customers/test_create_customer.py
@@ -1,8 +1,9 @@
 from Helpers.Customer_Helper import CustomerHelper
+import pdb
 
 
 def test_create_customer():
     customer_helper = CustomerHelper()
     customer_helper.create_customer()
+    pdb.set_trace()
     customer_helper.verify_created_email()
-
