03b66936d5b1e967878bbc89b198f638b7c2fbdc
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-17 11:36:15 +0530

    helper function added.

--- Test_Cases/Customers/test_get_customers.py
+++ Test_Cases/Customers/test_get_customers.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @pytest.mark.customer_sanity
-@pytest.mark.tcid_get_customer_2
+@pytest.mark.tcid_get_customer_1
 def test_get_all_customers(woo_api_test):
     customer_helper = CustomerHelper()
     customer_helper.get_customers()
