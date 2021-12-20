0f9d9464b9b0456b3e5c76de56b236b31077efec
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-20 19:35:02 +0530

    helper function added.

--- Helpers/Customer_Helper.py
+++ Helpers/Customer_Helper.py
@@ -1,7 +1,10 @@
 import json
+import logging
 from Utilities.data_generator import generate_email_password
+from Utilities.logging_utility import Logger
 from Utilities.requests_utility import RequestsUtility
-import logging as logger
+
+log = Logger(__name__, logging.INFO)
 
 
 class CustomerHelper:
@@ -17,21 +20,25 @@ class CustomerHelper:
         generator = generate_email_password(domain='mytest.com.au', email_prefix='runtest')
         self.email = generator['email']
         self.password = generator['password']
+        log.logger.info(f"Generated email is {self.email} and generated password is {self.password}")
         payload = dict()
         payload['email'] = self.email
         payload['password'] = self.password
         payload.update(kwargs)
         post_response = self.requests_util.post('customers', payload=json.dumps(payload), expected_status_code=201)
         self.post_response_json = post_response.json()
+        log.logger.info(f"post response is {self.post_response_json}")
         return self.post_response_json
 
     def verify_created_email(self):
         assert self.email == self.post_response_json['email'], f"The given email {self.email} " \
                                                                f"is not matching with the response email {self.post_response_json['email']}"
+        log.logger.info("Email verification completed.")
 
     def get_customers(self, **kwargs):
         get_response = self.requests_util.get('customers', expected_status_code=200)
         self.get_response_json = get_response.json()
+        log.logger.info(f"Get response received {self.get_response_json}")
         return self.get_response_json
 
     def create_existing_customer(self, existing_email):
@@ -39,5 +46,5 @@ class CustomerHelper:
         payload['email'] = existing_email
         payload['password'] = 'test1234'
         post_response = self.requests_util.post('customers', payload=json.dumps(payload), expected_status_code=400)
-        logger.info(f"Post response receive is {post_response.json()}")
+        log.logger.info(f"Post response receive is {post_response.json()}")
         return post_response
--- Utilities/requests_utility.py
+++ Utilities/requests_utility.py
@@ -1,9 +1,12 @@
+import logging
 import os
 from urllib.parse import urljoin
 import requests
 from Configuration.hosts_config import API_HOSTS, consumer_key, consumer_secret
 from requests_oauthlib import OAuth1
-import logging as logger
+from Utilities.logging_utility import Logger
+
+log = Logger(__name__, logging.INFO)
 
 
 class RequestsUtility:
@@ -19,12 +22,13 @@ class RequestsUtility:
         url = urljoin(self.base_url, endpoint)
         # url = self.base_url + endpoint
         post_response = requests.post(url=url, data=payload, headers=header, auth=self.auth)
+        log.logger.info("post response executed.")
         actual_status_code = post_response.status_code
         if not expected_status_code:
             actual_status_code = None
         assert expected_status_code == actual_status_code, \
             f"expected status code is {expected_status_code} but actual status code is {actual_status_code}"
-        logger.debug(f"API response is {post_response.json()}")
+        log.logger.info(f"API response is {post_response.json()}")
         return post_response
 
     def get(self, endpoint, header=None, expected_status_code=None):
@@ -37,7 +41,7 @@ class RequestsUtility:
             actual_status_code = None
         assert expected_status_code == actual_status_code, \
             f"expected status code is {expected_status_code} but actual status code is {actual_status_code}"
-        logger.debug(f"API response is {get_response.json()}")
+        log.logger.info(f"API response is {get_response.json()}")
         return get_response
 
     def update(self):
