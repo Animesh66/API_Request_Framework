819c98e04adeabc53e91eb20312057109cf76015
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-20 19:48:14 +0530

    helper function added.

--- Utilities/config_reader.py
+++
@@ -1,7 +0,0 @@
-from configparser import ConfigParser
-
-
-def config_reader(section, key):
-    config = ConfigParser()
-    config.read(".\\Configuration\\conf.ini")
-    return config.get(section, key)
